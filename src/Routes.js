0bc43c595898a7e444df4383c46c745887809411
Celestin Niyonsaba <niyoceles@gmail.com>
2020-06-25 15:08:25 +0200

    fixing conflicts

--- src/Routes.js
+++ src/Routes.js
@@ -1,26 +1,16 @@
 import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-<<<<<<< HEAD
-import { ForgotPasswordScreen, ResetPasswordScreen, DashboardScreen, CategoryScreen } from './screens';
+import { ForgotPasswordScreen, ResetPasswordScreen,
+    DashboardScreen, ArticleViewScreen, CategoryScreen, HomeScreen } from './screens';
+import CreateArticlePage from './pages/CreateArticlePage';
 import ArticlesScreen from './screens/article/index';
-import SetPasswordScreen from './screens/auth/password/set'
+import SetPasswordScreen from './screens/auth/password/set';
 import LoginScreen from './screens/auth/LoginScreen';
 import Home from './pages/Home.jsx';
 import CreateArticle from './components/Articles/CreateArticle';
 import ManageUsersPage from './pages/ManageUsersPage';
-export class Routes extends Component{
-=======
-import { 
-    ForgotPasswordScreen, ResetPasswordScreen, SetPasswordScreen, 
-    DashboardScreen, ArticleScreen, ArticleViewScreen, CategoryScreen,
-    HomeScreen 
-} from './screens';
-import LoginScreen from './screens/auth/LoginScreen';
-import CreateArticlePage from './pages/CreateArticlePage'
-export class Routes extends React.Component
-{
->>>>>>> 622c2bb... improve ui & visitor ui
+
+export class Routes extends React.Component{
     render() {
         return (
             <Router>
@@ -33,15 +23,10 @@ export class Routes extends React.Component
                         <Route exact path='/password/reset/:hash' component={ResetPasswordScreen} />
                         <Route exact path='/password/set/' component={SetPasswordScreen} />
                         <Route exact path='/dashboard' component={DashboardScreen} />
-<<<<<<< HEAD
                         <Route exact path='/articles' component={ArticlesScreen} />
                         <Route exact path='/articles/create' component={CreateArticle} />
-=======
-
-                        <Route exact path='/article' component={ArticleScreen} />
                         <Route exact path='/article/:id' component={ArticleViewScreen} />
                         <Route exact path='/articles/create' component={CreateArticlePage} />
->>>>>>> 622c2bb... improve ui & visitor ui
                         <Route exact path='/category' component={CategoryScreen} />
                     </Fragment>
                 </Switch>
